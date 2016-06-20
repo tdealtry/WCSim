800bf3dde38908f2523d41b423a9b3cad8de7729
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2016-06-20 17:10:48 +0100

    More TMVA application analysis outputs

--- analysistools/TMVA/MakeApplicationPlots.C
+++ analysistools/TMVA/MakeApplicationPlots.C
@@ -10,6 +10,11 @@
 #include "TEventList.h"
 #include "TMath.h"
 
+void DrawStacks(THStack * hs, TString opt, TString pdfname, TCanvas * c);
+TH1D * MakeHistogram(const int mode, const int ie,
+		     const TString hname, const TString htitle, const int nbins, const double xmax);
+TEventList * MakeEventList(TTree * t, const TString elistname, const TString ecut);
+
 void MakeApplicationPlots(TString variables = "Bdt,BdtB,BdtG,BdtD,Cuts", int verbose = 1, const char * filename = "TMVApp.root")
 {
   TFile f(filename);
@@ -22,12 +27,13 @@ void MakeApplicationPlots(TString variables = "Bdt,BdtB,BdtG,BdtD,Cuts", int ver
   cout << "Tree contains " << t->GetEntries() << " events" << endl;
 
   TFile fout("TMVAPlots.root", "RECREATE");
-  TCanvas c;
-  c.SetTopMargin(0.2);
-  c.SaveAs("TMVAPlots.pdf[");
+  TCanvas * c = new TCanvas();
+  c->SetTopMargin(0.2);
+  c->SaveAs("TMVAPlots.pdf[");
 
-  const int nvariables = 1;
+  const int nvariables = 10;
   const int nenergies  = 10;
+  //book histograms
   TH1D * h_value[nvariables][nenergies];
   THStack * hs_value[nvariables];
   TH1D * h_efficiency[nvariables][nenergies];
@@ -36,11 +42,13 @@ void MakeApplicationPlots(TString variables = "Bdt,BdtB,BdtG,BdtD,Cuts", int ver
   THStack * hs_purity[nvariables];
   TH1D * h_ep[nvariables][nenergies];
   THStack * hs_ep[nvariables];
+  //book everything else
+  double max_axis[nvariables];
   TEventList * eventlists[nenergies], * eventlists_sig[nenergies], * eventlists_bkg[nenergies];
   Long64_t nentries[nenergies];
-  double max_axis[nvariables];
+  //loop variables
   int iv = 0;
-  Ssiz_t from = 0;
+  Ssiz_t from = 0;  //dummy
   TString variable;
   while(variables.Tokenize(variable, from, ",")) {
     cout << variable << endl;
@@ -55,72 +63,46 @@ void MakeApplicationPlots(TString variables = "Bdt,BdtB,BdtG,BdtD,Cuts", int ver
     double min = t->GetMinimum(variable);
     double max = t->GetMaximum(variable);
     max_axis[iv] = TMath::Ceil(TMath::Max(TMath::Abs(min), TMath::Abs(max)));
+
     for(int ie = 0; ie < nenergies; ie++) {
       //apply energy cut
       if(iv == 0) {
-	t->SetEventList(0);
-	TString elistname = TString::Format("elist%d", ie);
-	eventlists[ie] = new TEventList(elistname);
-	t->Draw(TString::Format(">>%s", elistname.Data()),
-		TString::Format("TRUE_e_energy>=%d&&TRUE_e_energy<%d", ie, ie+1));
+	eventlists[ie] = MakeEventList(t, TString::Format("elist%d", ie),
+				       TString::Format("TRUE_e_energy>=%d&&TRUE_e_energy<%d", ie, ie+1));
 	nentries[ie] = eventlists[ie]->GetN();
-	cout << "Event list " << elistname << " has " << nentries[ie] << " events" << endl;
 
 	//and for the purities...
-	t->SetEventList(0);
-	TString elistname_sig = TString::Format("elist_sig%d", ie);
-	eventlists_sig[ie] = new TEventList(elistname_sig);
-	t->Draw(TString::Format(">>%s", elistname_sig.Data()), TString::Format("TRUE_e_energy>=%d", ie));
-	t->SetEventList(0);
-	TString elistname_bkg = TString::Format("elist_bkg%d", ie);
-	eventlists_bkg[ie] = new TEventList(elistname_bkg);
-	t->Draw(TString::Format(">>%s", elistname_bkg.Data()), TString::Format("TRUE_e_energy<%d", ie));
+	eventlists_sig[ie] = MakeEventList(t, TString::Format("elist_sig%d", ie),
+					   TString::Format("TRUE_e_energy>=%d", ie));
+	eventlists_bkg[ie] = MakeEventList(t, TString::Format("elist_bkg%d", ie),
+					   TString::Format("TRUE_e_energy<%d", ie));
       }
-      t->SetEventList(eventlists[ie]);
 
-      //make the histograms pretty
+      //make the histograms & make them pretty
       TString htitle_ecut = TString::Format("E = %d-%d MeV", ie, ie+1);
       TString htitle_ecut_purity = TString::Format("E >= %d MeV", ie);
       //value
-      TString hname = TString::Format("h_value_%s_e%d", variable.Data(), ie);
-      h_value[iv][ie] = new TH1D(hname, htitle_ecut, 100, -max_axis[iv], +max_axis[iv]);
-      h_value[iv][ie]->SetFillColor(1 + ie);
-      h_value[iv][ie]->SetMarkerColor(h_value[iv][ie]->GetLineColor());
-      h_value[iv][ie]->SetMarkerStyle(1);
-      //efficiency
-      hname = TString::Format("h_efficiency_%s_e%d", variable.Data(), ie);
-      h_efficiency[iv][ie] = new TH1D(hname, htitle_ecut, 100, -max_axis[iv], +max_axis[iv]);
-      h_efficiency[iv][ie]->SetLineColor(ie == 9 ? kGray : 1 + ie);
-      h_efficiency[iv][ie]->SetMarkerColor(h_efficiency[iv][ie]->GetLineColor());
-      h_efficiency[iv][ie]->SetMarkerStyle(1);
-      //purity
-      hname = TString::Format("h_purity_%s_e%d", variable.Data(), ie);
-      h_purity[iv][ie] = new TH1D(hname, htitle_ecut_purity, 100, -max_axis[iv], +max_axis[iv]);
-      h_purity[iv][ie]->SetLineColor(ie == 9 ? kGray : 1 + ie);
-      h_purity[iv][ie]->SetMarkerColor(h_purity[iv][ie]->GetLineColor());
-      h_purity[iv][ie]->SetMarkerStyle(1);
-      //ep
-      hname = TString::Format("h_ep_%s_e%d", variable.Data(), ie);
-      h_ep[iv][ie] = new TH1D(hname, htitle_ecut, 100, -max_axis[iv], +max_axis[iv]);
-      h_ep[iv][ie]->SetLineColor(ie == 9 ? kGray : 1 + ie);
-      h_ep[iv][ie]->SetMarkerColor(h_ep[iv][ie]->GetLineColor());
-      h_ep[iv][ie]->SetMarkerStyle(1);
+      h_value[iv][ie] = MakeHistogram(0, ie, TString::Format("h_value_%s_e%d", variable.Data(), ie),
+				      htitle_ecut, 100, max_axis[iv]);
+      h_efficiency[iv][ie] = MakeHistogram(1, ie, TString::Format("h_efficiency_%s_e%d", variable.Data(), ie),
+					   htitle_ecut, 100, max_axis[iv]);
+      h_purity[iv][ie] = MakeHistogram(1, ie, TString::Format("h_purity_%s_e%d", variable.Data(), ie),
+				       htitle_ecut_purity, 100, max_axis[iv]);
+      h_ep[iv][ie] = MakeHistogram(1, ie, TString::Format("h_ep_%s_e%d", variable.Data(), ie),
+				   htitle_ecut, 100, max_axis[iv]);
 
       //fill the value histogram
-      t->Draw(TString::Format("%s>>%s", variable.Data(), hname.Data()),
+      cout << "Event list " << eventlists[ie]->GetTitle() << " has " << nentries[ie] << " events" << endl;
+      t->SetEventList(0);
+      t->SetEventList(eventlists[ie]);
+      t->Draw(TString::Format("%s>>%s", variable.Data(), h_value[iv][ie]->GetName()),
 	      "", "goff");
-
-      //fill the efficiency histogram
-      for(int ib = 1; ib <= h_efficiency[iv][ie]->GetNbinsX(); ib++) {
-	//cout << h_efficiency[iv][ie]->GetBinLowEdge(ib) << "\t"
-	//   << nentries_passed << "\t" << nentries[ie] << "\t" << efficiency << endl;
-      }//ib
 
       //fill the efficiency & purity & ep histograms
       for(int ib = 1; ib <= h_purity[iv][ie]->GetNbinsX(); ib++) {
+	t->SetEventList(eventlists[ie]);
 	TString varcut = TString::Format("%s>=%f", variable.Data(), h_purity[iv][ie]->GetBinLowEdge(ib));
 	//efficiency
-	t->SetEventList(eventlists[ie]);
 	Long64_t nentries_passed = t->GetEntries(varcut);
 	double efficiency = (double)nentries_passed / (double)nentries[ie];
 	h_efficiency[iv][ie]->SetBinContent(ib, efficiency);
@@ -132,7 +114,7 @@ void MakeApplicationPlots(TString variables = "Bdt,BdtB,BdtG,BdtD,Cuts", int ver
 	double purity = ((nentries_passed_sig + nentries_passed_bkg == 0) ? 0 
 			 : ((double)nentries_passed_sig / (double)(nentries_passed_sig + nentries_passed_bkg)));
 	h_purity[iv][ie]->SetBinContent(ib, purity);
-
+	//ep
 	h_ep[iv][ie]->SetBinContent(ib, efficiency * purity);
 
 	if(verbose > 2)
@@ -143,10 +125,16 @@ void MakeApplicationPlots(TString variables = "Bdt,BdtB,BdtG,BdtD,Cuts", int ver
       }//ib
 
       //populate the stacks
-      hs_value[iv]->Add(h_value[iv][ie]);
+      hs_value     [iv]->Add(h_value[iv][ie]);
       hs_efficiency[iv]->Add(h_efficiency[iv][ie]);
-      hs_purity[iv]->Add(h_purity[iv][ie]);
-      hs_ep[iv]->Add(h_ep[iv][ie]);
+      hs_purity    [iv]->Add(h_purity[iv][ie]);
+      hs_ep        [iv]->Add(h_ep[iv][ie]);
+
+      //save the histograms
+      h_value     [iv][ie]->Write();
+      h_efficiency[iv][ie]->Write();
+      h_purity    [iv][ie]->Write();
+      h_ep        [iv][ie]->Write();
 
       //cout << "finishing energy loop " << ie << endl;
     }//ie
@@ -154,43 +142,54 @@ void MakeApplicationPlots(TString variables = "Bdt,BdtB,BdtG,BdtD,Cuts", int ver
 
 
     //draw the histograms & save them
-    hs_value[iv]->Write();
-    hs_efficiency[iv]->Write();
-    hs_purity[iv]->Write();
-
-    hs_value[iv]->Draw();
-    hs_value[iv]->GetYaxis()->SetTitleOffset(1.1);
-    TLegend * l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), 1 - gPad->GetRightMargin(), 1.0, "");//, "F");
-    l->SetNColumns(5);
-    gPad->Update();
-    c.SaveAs("TMVAPlots.pdf");
-
-    hs_efficiency[iv]->Draw("NOSTACK");
-    hs_efficiency[iv]->GetYaxis()->SetTitleOffset(1.1);
-    l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), 1 - gPad->GetRightMargin(), 1.0, "");//, "F");
-    l->SetNColumns(5);
-    gPad->Update();
-    c.SaveAs("TMVAPlots.pdf");
-
-    hs_purity[iv]->Draw("NOSTACK");
-    hs_purity[iv]->GetYaxis()->SetTitleOffset(1.1);
-    l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), 1 - gPad->GetRightMargin(), 1.0, "");//, "F");
-    l->SetNColumns(5);
-    gPad->Update();
-    c.SaveAs("TMVAPlots.pdf");
-
-    hs_ep[iv]->Draw("NOSTACK");
-    hs_ep[iv]->GetYaxis()->SetTitleOffset(1.1);
-    l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), 1 - gPad->GetRightMargin(), 1.0, "");//, "F");
-    l->SetNColumns(5);
-    gPad->Update();
-    c.SaveAs("TMVAPlots.pdf");
+    DrawStacks(hs_value[iv], "", "TMVAPlots.pdf", c);
+    DrawStacks(hs_efficiency[iv], "NOSTACK", "TMVAPlots.pdf", c);
+    DrawStacks(hs_purity[iv], "NOSTACK", "TMVAPlots.pdf", c);
+    DrawStacks(hs_ep[iv], "NOSTACK", "TMVAPlots.pdf", c);
 
     //loop
     iv++;
   }//loop over variables
 
-  c.SaveAs("TMVAPlots.pdf]");
+  c->SaveAs("TMVAPlots.pdf]");
   fout.Close();
   f.Close();
+}
+
+void DrawStacks(THStack * hs, TString opt, TString pdfname, TCanvas * c)
+{
+  hs->Write();
+  hs->Draw(opt);
+  hs->GetYaxis()->SetTitleOffset(1.1);
+  TLegend * l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), 1 - gPad->GetRightMargin(), 1.0, "");//, "F");
+  l->SetNColumns(5);
+  gPad->Update();
+  c->SaveAs(pdfname);
+}
+
+
+TH1D * MakeHistogram(const int mode, const int ie,
+		     const TString hname, const TString htitle, const int nbins, const double xmax)
+{
+  TH1D * h = new TH1D(hname, htitle, nbins, -xmax, +xmax);
+
+  if(mode == 0) {
+    h->SetFillColor(1 + ie);
+    h->SetMarkerColor(h->GetLineColor());
+    h->SetMarkerStyle(1);
+  }
+  else if(mode == 1) {
+    h->SetLineColor(ie == 9 ? kGray : 1 + ie);
+    h->SetMarkerColor(h->GetLineColor());
+    h->SetMarkerStyle(1);
+  }
+  return h;
+}
+
+TEventList * MakeEventList(TTree * t, const TString elistname, const TString ecut)
+{
+  TEventList * elist = new TEventList(elistname, ecut);
+  t->SetEventList(0);
+  t->Draw(TString::Format(">>%s", elistname.Data()), ecut);
+  return elist;
 }
