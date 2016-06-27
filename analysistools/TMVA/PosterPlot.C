dd4a0bea3bf7ab800fc54dc493ba8bb6372293a3
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2016-06-27 12:52:21 +0100

    Update the poster plotter - change axis labels! Add a .gitignore for the TMVA outputs

--- analysistools/TMVA/PosterPlot.C
+++ analysistools/TMVA/PosterPlot.C
@@ -1,14 +1,39 @@
+#include "TFile.h"
+#include "THStack.h"
+#include "TCanvas.h"
+#include "TList.h"
+#include "TCollection.h"
+#include "TH1D.h"
+#include "TLegend.h"
+
+#include <iostream>
+
 void PosterPlot(const char * pdfname = "tmvaplot.pdf", const char * filename = "TMVAPlots.root", const char * stackname = "hs_value_Bdt", const char * drawopt = "")
 {
   TFile f(filename);
   THStack * hs = 0;
   f.GetObject(stackname, hs);
 
+  TList * list = hs->GetHists();
+  TIter next(list);
+  TObject * o;
+  TH1D * htemp;
+  while(o = next.Next()) {
+    htemp = (TH1D*)o;
+    htemp->GetXaxis()->SetTitle("Separation Variable");
+    //htemp->SetFillColor(kBlue);
+    cout << htemp << endl;
+  }
+
+
   TCanvas * c = new TCanvas();
-  c->DrawFrame(-1,0,1,40000);
+  hs->SetTitle("bdt;Separation variable;Number of events");
+  hs->SetMaximum(40000 * 0.97);
   hs->Draw(TString::Format("%s", drawopt));
   hs->GetYaxis()->SetNoExponent(false);
   hs->GetYaxis()->SetTitleOffset(1.1);
+  hs->Draw(TString::Format("%s", drawopt));
+  hs->Dump();
 
   /*
   // legend at the top
@@ -26,16 +51,16 @@ void PosterPlot(const char * pdfname = "tmvaplot.pdf", const char * filename = "
   */
 
   //legend over text
-  c->SetRightMargin(0.01);
-  c->SetTopMargin(0.01);
+  c->SetRightMargin(0.02);
+  c->SetTopMargin(0.02);
   c->SetBottomMargin(0.13);
-  TLegend * l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), gPad->GetLeftMargin() + 0.5, 1 - gPad->GetTopMargin() - 0.4, "");
+  TLegend * l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), 0.7, 0.62);
+  //TLegend * l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), gPad->GetLeftMargin() + 0.5, 1 - gPad->GetTopMargin() - 0.4, "");
   //TLegend * l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), gPad->GetLeftMargin() + 0.3, 1 - gPad->GetTopMargin() - 0.5, "");
   //TLegend * l = gPad->BuildLegend(gPad->GetLeftMargin(), 1 - gPad->GetTopMargin(), gPad->GetLeftMargin() + 0.3, 1 - gPad->GetTopMargin() - 0.5, "");
   l->SetNColumns(2);
   l->SetTextFont(132);
   l->SetMargin(0.2);
-  hs->GetYaxis()->SetRangeUser(0,40000);
 
   gPad->Update();
   c->SaveAs(pdfname);
